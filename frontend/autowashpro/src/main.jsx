2fa395c4c026e21aa6238ced23639bc5b571c14b
Gabriel-hp <gdias6187@gmail.com>
2024-09-30 15:29:51 -0400

    UPDATE

---
+++ frontend/autowashpro/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '/xampp/htdocs/projeto-lavajato-react/frontend/autowashpro/src/App.jsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
